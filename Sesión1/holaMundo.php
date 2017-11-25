c5a355077d02394b9e65c1c16e696333635dd79a
kubos777 <chd.jl@hotmail.com>
2017-11-25 15:14:46 -0600

    :neckbeard: Agrego códigos sobre variables, constantes y arreglos.

---
+++ Sesión1/holaMundo.php
@@ -0,0 +1,21 @@
+<?php 
+//echo sirve para mostrar información en el navegador
+#Otra forma de agregar comentarios
+/*
+Comentarios de bloque
+*/
+echo "Hola mundo de PHP";
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Iniciando con PHP</title>
+ </head>
+ <body>
+ 	<h1>Hola mundo soy un h1</h1>
+ 	<h1><?php echo "Hola mundo con PHP, dentro de un h1"; ?></h1>
+ </body>
+ </html>
